1d516a500dda3389c95f938b1ad4048d92f63bb9
CodeNinjasEtobicoke <codeninjasetobicoke@outlook.com>
2024-07-27 13:47:50 -0400

    Logging json file

--- app.js
+++ app.js
@@ -1,23 +1,41 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 
 // Handle GET requests to the root path
 app.get('/', function(req, res) {
-    console.log("Received a request from test...!!!");
+
     res.sendFile(__dirname + '/public/index.html');
 });
 
 app.get('/api', function(req, res) {
-    console.log("Received a request from test...!!!");
+
     res.sendFile(__dirname + '/public/api.html');
 });
 
-// document.querySelector('#retrieveData').addEventListener('click', () => {
-//     let rawdata = readFileSync('./data/items.json');
-//     let equipment = JSON.parse(rawdata);
-//     console.log(equipment);
-//     document.dispatchEvent(myEvent);
-// });
+const read = async () => {
+    console.log("reading")
+    const rawdata = fs.readFile("data/items.json");
+    let equipment = JSON.parse(rawdata);
+    console.log(equipment);
+  }
+
+app.get('/retrieveData', function(req, res) {
+    console.log("Received a request from test...!!!");
+
+
+    fs.readFile(__dirname + '/data/items.json', 'utf8', (err, data) => {
+        if (err) 
+        {
+            console.error(err);
+            return;
+        }
+        let equipment = JSON.parse(data);
+        console.log(equipment);
+    });
+    res.sendFile(__dirname + '/public/api.html');
+    
+});
 
 
 app.listen(3000, function () {
